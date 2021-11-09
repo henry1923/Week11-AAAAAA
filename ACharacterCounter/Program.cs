791c16193606fa1664a031f516b2c45910b100b3
Opilane TTHK <opilane@tthk.ee>
2021-11-09 15:48:10 +0200

    1

---
+++ ACharacterCounter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System/;
+
+namespace ACharacterCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SISESTA OMA EESNIMI: ");
+            string userFirstName = Console.ReadLine();
+
+            Console.WriteLine("SISESTA OMA PEREKONNANIMI.");
+            string userLastName = Console.ReadLine();
+
+            string fullName = $"{userFirstName}{userLastName}";
+
+            int counter = 0;
+
+            foreach(char character in fullName)
+            {
+                if (character == 'a') ;
+            }   
+
+            {
+
+                counter++;
+                    
+            }
+
+            Console.WriteLine($"sinu nimes on {counter} a-tähte");
+
+
+        }
+    }
+}
---
+++ HOROSCOPE/HOROSCOPE/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your horoscope sign:");
+            string userSign = Console.ReadLine();
+            Console.WriteLine(GetHoroscope(userSign));
+
+
+
+        }
+        //a function to open and read the file
+        public static string[] ReadHoroscope()
+        {
+            string filepath = @"C:\Users\opilane\samples\horoscope.txt"; //absolute file path
+            string[] DataFromFile = File.ReadAllLines(filepath);
+            Console.WriteLine(DataFromFile.Length);
+
+            return DataFromFile;
+        }
+
+        public static string GetHoroscope(string userInput)
+        {
+            string[] horoscope = ReadHoroscope();
+            string todayHoroscope = "";
+
+            foreach (string element in horoscope)
+            {
+                if (element.Contains(userInput))
+                {
+                    todayHoroscope = element;
+                }
+            }
+
+            return todayHoroscope;
+        }
+
+
+
+
+    }
+}
